40792dbf1d24320640b663f78446ded69b495914
Kratika Parmar <kratika.parmar1904@gmail.com>
2026-07-03 14:15:02 +0530

    learned variables

---
+++ 01-basics/variables.py
@@ -0,0 +1,58 @@
+## variable assignment
+speed = 80
+temprature = 104
+ecu_name = "Engine Control ECU"
+is_enginer_running = True
+
+print(speed)
+print(temprature)
+print(ecu_name)
+print(is_enginer_running)
+
+##################
+
+## variable update
+print("Variable update result")
+speed = 80
+print(speed)
+
+speed = 100
+print(speed)
+
+speed = 120
+print(speed)
+###########################
+
+##Multiple Assignment
+x, y, z = 10, 20, 30
+
+print("Multiple Assignment result")
+print(x)
+print(y)
+print(z)
+###########################
+
+##Swapping
+a = 10
+b = 20
+
+a, b = b, a
+print("swapping result")
+print(a)
+print(b)
+###########################
+
+##Developer profile
+name = "Kratika Parmar"
+company = "Bosch"
+experience = 12
+role = "Tech Lead"
+domain = "Automotive Embedded Software"
+learning_goal = "Python for Embedded Systems"
+
+print("Name :", name)
+print("Company :", company)
+print("Experience :", experience)
+print("Role :", role)
+print("Domain :", domain)
+print("Learning Goal :", learning_goal)
